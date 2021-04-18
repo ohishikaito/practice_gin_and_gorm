2db419615a1efc441b94317c9b8263940f3f3c73
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-04-18 13:16:51 +0900

    cretateに追加したり

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,13 +11,80 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-func init() {
-	fmt.Println("init????????")
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
 }
 
 func main() {
-	db := sqlConnect()
-	defer db.Close()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, q *http.Request) {
+		message := map[string]string{
+			"message": "hello world",
+		}
+		jsonMessage, err := json.Marshal(message)
+	})
+
+	// engine := gin.Default()
+	// engine.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "hello world",
+	// 	})
+	// })
+	// engine.Run()
+
+	// // Qiitaでやったやつら！簡単な方
+	// db := sqlConnect()
+	// db.AutoMigrate(&User{})
+	// defer db.Close()
+
+	// router := gin.Default()
+	// router.LoadHTMLGlob("templates/*.html")
+
+	// router.GET("/", func(ctx *gin.Context) {
+	// 	db := sqlConnect()
+	// 	var users []User
+	// 	res := db.Order("created_at asc").Find(&users)
+	// 	fmt.Println(res)
+	// 	defer db.Close()
+
+	// 	ctx.HTML(200, "index.html", gin.H{
+	// 		"users": users,
+	// 	})
+	// })
+
+	// router.POST("/create", func(ctx *gin.Context) {
+	// 	db := sqlConnect()
+	// 	name := ctx.PostForm("name")
+	// 	email := ctx.PostForm("email")
+	// 	fmt.Printf("create uesr name = %s, email = %s \n", name, email)
+	// 	db.Create(&User{
+	// 		Name:  name,
+	// 		Email: email,
+	// 	})
+	// 	defer db.Close()
+
+	// 	ctx.Redirect(302, "/")
+	// })
+
+	// router.DELETE("/delete/:id", func(ctx *gin.Context) {
+	// 	db := sqlConnect()
+	// 	n := ctx.Param("id")
+	// 	id, err := strconv.Atoi(n)
+	// 	if err != nil {
+	// 		return
+	// 	}
+	// 	var user User
+	// 	db.First(&user, id)
+	// 	db.Delete(&user)
+	// 	defer db.Close()
+
+	// 	ctx.Redirect(302, "/")
+	// })
+
+	// // router.Runしないと認識しないからね！
+	// router.Run()
 
 	// openWebPage()
 }
@@ -24,15 +93,12 @@ func sqlConnect() (database *gorm.DB) {
 	DBMS := "mysql"
 	USER := "root"
 	PASS := "root"
-	// protocolはtcp(db:)で良いのか？？サービス名とコンテナ名にしろっていう記述が乱立してて草
-	// PROTOCOL := "tcp(db:3308)"
-	// つながらないからコンテナ名にした
-	// PROTOCOL := "tcp(go_myapp_db_1:3308)"
+	// dockerのネットワークを経由してアクセスするから、コンテナ名にしろ
 	PROTOCOL := "tcp(go_myapp_db_1)"
 	DBNAME := "go_database"
 
 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-	fmt.Println(CONNECT)
+	// fmt.Println(CONNECT)
 
 	count := 0
 	db, err := gorm.Open(DBMS, CONNECT)
