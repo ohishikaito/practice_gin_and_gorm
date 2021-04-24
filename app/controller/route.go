74d21fbc051c85309bc9aec221baadf59259d709
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-04-25 00:54:26 +0900

    a

--- app/controller/route.go
+++ app/controller/route.go
@@ -1,27 +1,39 @@
 package controller
 
-// func (app *App) successResponse(w http.ResponseWriter, body interface{}, statusCode int) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(statusCode)
-// 	json.NewEncoder(w).Encode(body)
-// }
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
-// func (app *App) errorResponse(w http.ResponseWriter, Response string, statusCode int) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.WriteHeader(statusCode)
-// 	type JSONResponse struct {
-// 		Response string `json:"response"`
-// 	}
-// 	jsonError, err := json.Marshal(JSONResponse{Response: Response})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	w.Write(jsonError)
-// }
+	"github.com/gorilla/mux"
+)
 
-// func HttpRoute(app *App) *mux.Router {
-// 	r := mux.NewRouter()
-// 	r.HandleFunc("/users", app.UserIndexHandler).Methods("GET")
-// 	r.HandleFunc("/users", app.userCreateHandler).Methods("POST")
-// 	return r
-// }
+func NewApp() *App {
+	return &App{}
+}
+
+func (app *App) successResponse(w http.ResponseWriter, body interface{}, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(body)
+}
+
+func (app *App) errorResponse(w http.ResponseWriter, Response string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	type JSONResponse struct {
+		Response string `json:"response"`
+	}
+	jsonError, err := json.Marshal(JSONResponse{Response: Response})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonError)
+}
+
+func HttpRoute(app *App) *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/users", app.UserIndexHandler).Methods("GET")
+	r.HandleFunc("/users", app.userCreateHandler).Methods("POST")
+	return r
+}
--- app/controller/user.go
+++ app/controller/user.go
@@ -1,30 +1,37 @@
 package controller
 
-// var userService service.UserService
+import (
+	"encoding/json"
+	"go_myapp/app/model"
+	"go_myapp/app/service"
+	"net/http"
+)
 
-// func (app *App) UserIndexHandler(w http.ResponseWriter, r *http.Request) {
-// 	users, _ := userService.GetUserIndex()
-// 	type data struct {
-// 		Data []model.User `json:"data"`
-// 	}
-// 	body := data{
-// 		Data: users,
-// 	}
-// 	app.successResponse(w, body, http.StatusOK)
-// }
+var userService service.UserService
 
-// func (app *App) userCreateHandler(w http.ResponseWriter, r *http.Request) {
-// 	var user model.User
-// 	json.NewDecoder(r.Body).Decode(&user)
-// 	if err := userService.CreateUser(&user); err != nil {
-// 		app.errorResponse(w, err.Error(), http.StatusUnprocessableEntity)
-// 		return
-// 	}
-// 	type data struct {
-// 		Data model.User `json:"data"`
-// 	}
-// 	body := data{
-// 		Data: user,
-// 	}
-// 	app.successResponse(w, body, http.StatusCreated)
-// }
+func (app *App) UserIndexHandler(w http.ResponseWriter, r *http.Request) {
+	users, _ := userService.GetUserIndex()
+	type data struct {
+		Data []model.User `json:"data"`
+	}
+	body := data{
+		Data: users,
+	}
+	app.successResponse(w, body, http.StatusOK)
+}
+
+func (app *App) userCreateHandler(w http.ResponseWriter, r *http.Request) {
+	var user model.User
+	json.NewDecoder(r.Body).Decode(&user)
+	if err := userService.CreateUser(&user); err != nil {
+		app.errorResponse(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+	type data struct {
+		Data model.User `json:"data"`
+	}
+	body := data{
+		Data: user,
+	}
+	app.successResponse(w, body, http.StatusCreated)
+}
