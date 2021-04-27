3360fb39f6f4978d20b0a2a53ffa40dd3caf17bd
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-04-27 09:36:45 +0900

    Update book.go

--- app/model/book.go
+++ app/model/book.go
@@ -6,9 +6,9 @@ import (
 
 type Book struct {
 	gorm.Model
-	Price      int    `validate:"required,max=100"`
-	Title      string `validate:"required"`
-	Content    string `validate:"required"`
-	BookDetail BookDetail
+	Price      int        `validate:"required,max=100"`
+	Title      string     `validate:"required"`
+	Content    string     `validate:"required"`
+	BookDetail BookDetail `validate:"omitempty,nostructlevel"`
 	Comments   []Comment
 }
