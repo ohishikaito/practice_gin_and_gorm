2ac517cd27427f8c2877c976fdb8e35193f98a95
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-04-28 09:42:48 +0900

    ha?

---
+++ auth/auth.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["admin"] = true
+	claims["sub"] = "54546557354"
+	claims["name"] = "taro"
+	claims["iat"] = time.Now()
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokenString, _ := token.SignedString([]byte("HOGE"))
+	fmt.Println(tokenString)
+	w.Write([]byte(tokenString))
+})
+
+// JwtMiddleware check token
+var JwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return []byte("HOGE"), nil
+	},
+	SigningMethod: jwt.SigningMethodHS256,
+})
